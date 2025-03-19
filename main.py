233e58ea1a03cbff27a0f14bc3ce6d8238343b8f
sandhyamainali <sandhyamainali55@gmail.com>
2025-03-19 13:58:16 +0545

    Python Notes

---
+++ main.py
@@ -0,0 +1,6 @@
+from mathematics.arithmetic import sum,diff,mult,modulus
+result= sum(3,5)
+print("Difference of value is:",diff(12,1))
+print("Sum of value is:",result)
+print("Multiplication of values is:",mult(12,2))
+print("Value of modulus is:",modulus(14,2))
---
+++ mathematics/arithmetic.py
@@ -0,0 +1,15 @@
+
+def sum(x,y):
+    return x + y
+print("Sum of value:",sum(12,11))
+def diff(x,y):
+    return x-y
+print("Difference of values:",diff(12,2))
+
+def mult(x,y):
+    return x * y
+print("Multiplication of value:",mult(11,3))
+
+def modulus(x,y):
+    return x % y
+print("modulus of value :", modulus(12,6))
